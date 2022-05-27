851f81e00c36a95368d1133ed7b343d3320b382e
Willians Ribeiro <willians.filho@bandtec.com.br>
2022-05-27 15:45:49 -0300

    add alterações dashboard

--- site/public/js/navDash.js
+++ site/public/js/navDash.js
@@ -1,47 +1,47 @@
 var menuToggle = document.querySelector('.toggle-menu');
 
-if(window.matchMedia("(min-width: 1200px)").matches){
+if (window.matchMedia("(min-width: 1200px)").matches) {
 
-    var tlmenu = new TimeLineMax({
+    var tlmenu = new TimelineMax({
         paused: true
     })
 
     tlmenu.fromTo(".nav .menu li span, .logo span", 0.2,
-    {
-        opacity: 1,
-        display: "block"
-    },
-    {
-        opacity: 0,
-        display: "none",
-        ease: "power2.out"
-    }
+        {
+            opacity: 1,
+            display: "block"
+        },
+        {
+            opacity: 0,
+            display: "none",
+            ease: "power2.out"
+        }
     )
 
     tlmenu.fromTo(".logo .bi", 0.3,
-    {css: {marginRight: "15px"}},
-    {css: {marginRight: 0}, ease: "power2.out"},
-    .01
+        { css: { marginRight: "15px" } },
+        { css: { marginRight: 0 }, ease: "power2.out" },
+        .01
     )
-    
+
     tlmenu.fromTo(".nav", 0.5,
-     {width: "280px"},
-     {width: "80px", ease: "power2.out"},
-     .1
+        { width: "280px" },
+        { width: "80px", ease: "power2.out" },
+        .1
     )
 
     tlmenu.fromTo(".content, .footer", 0.5,
-     {width: "calc(100% - 280px)"},
-     {width: "calc(100% - 80px)", ease: "power2.out"},
-     .1
-     )
+        { width: "calc(100% - 280px)" },
+        { width: "calc(100% - 80px)", ease: "power2.out" },
+        .1
+    )
 
-     menuToggle.onclick = () => {
-         menuToggle.classList.toggle('active'); 
-          if(menuToggle.classList.contains('active')) {
+    menuToggle.onclick = () => {
+        menuToggle.classList.toggle('active');
+        if (menuToggle.classList.contains('active')) {
             tlmenu.reverse(0)
-          } else {
-              tlmenu.play(0)
-          }  
-     }
+        } else {
+            tlmenu.play(0)
+        }
+    }
 }
