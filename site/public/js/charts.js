b4ff8e37cf5effec1b663b7e5746ea0ff8e73c1d
WilliansMiguelRibeiro <willians.filho@bandtec.com.br>
2022-05-29 13:40:59 -0300

    Conexão BD e resposta em tempo real (Dashboard)

--- site/public/js/charts.js
+++ site/public/js/charts.js
@@ -1,116 +1,175 @@
+// Respostas do banco
 var cpuDashboard = [];
 var memoryDashboard = [];
+var discoDashboard = [];
 
-window.onload = function plotarGrafico() {
-  fetch("http://localhost:8080/", { method: "GET" })
+// Labels dos gráficos
+var labelDesempenhoCpu = [];
+var labelMemoria = [];
+var labelDisco = [];
+
+// Pegando o elemento na DOM para o gráfico
+var desempenhoCpu = document.getElementById('chartCpu').getContext('2d');
+var capacidadeMemoria = document.getElementById('chartMemoria').getContext('2d');
+var consumoDisco = document.getElementById('chartDisco').getContext('2d');
+
+
+function plotarGrafico() {
+  cpuDashboard = [];
+  labelDesempenhoCpu = [];
+  memoryDashboard = [];
+  labelMemoria = [];
+  discoDashboard = [];
+  labelDisco = [];
+
+
+  fetch("http://localhost:8080/dashboard/", { method: "GET" })
     .then(response => response.json())
     .then(result => {
-       result.forEach(element => {
-         console.log(cpuDashboard);
-         cpuDashboard.push(element.consumoCPU);
-       });
-       createCharts();
-    })
+      result.forEach(element => {
+        var formatedHour = element.hora.replace('Z', '');
+
+        var date = new Date(formatedHour);
+        var dateString = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`
+
+        labelDesempenhoCpu.push(dateString);
+        cpuDashboard.push(element.consumoCPU);
+
+        labelMemoria.push(dateString);
+        memoryDashboard.push(((element.consumoMemoriaEmBytes / 1024) / 1024));
+
+        labelDisco.push(dateString);
+        discoDashboard.push(((element.consumoDiscoEmBytes / 1024) /1024))
+      });
+      updateCharts();
+    }
+  );
 }
 
-var desempenhoCpu = document.getElementById('chartCpu').getContext('2d');
-var chart = new Chart(desempenhoCpu, {
-    type: 'line',
-    data: {
-        labels: ['0 s', '', '', '', '', '', '', '', '', '', '', '60 s'],
-        datasets: [
-            {
-                label: "",
-                backgroundColor: '#fff',
-                borderColor: '#38d6eb',
-                data: cpuDashboard,
-            }
-        ]
-    },
-    options: {
-        plugins: {
-          title: {
-            display: true,
-            text: 'Consumo da CPU',
-            padding: {
-              top: 10,
-            },
-            font: {
-              size: 18
-            }
+setInterval(() => {
+  plotarGrafico();
+}, 3000);
+
+function updateCharts() {
+
+  // tratando as labels
+  labelDesempenhoCpu.reverse();
+  labelMemoria.reverse();
+  labelDisco.reverse();
+
+  // Atualizando os dados do primeiro gráfico
+  chartDesempenhoCPU.data.labels = labelDesempenhoCpu;
+  chartDesempenhoCPU.data.datasets[0].data = cpuDashboard;
+
+  // Atualizando os dados do segundo gráfico
+  chartMemoria.data.labels = labelMemoria;
+  chartMemoria.data.datasets[0].data = memoryDashboard;
+
+  // Atualizando os dados do terceiro gráfico
+  chartDisco.data.labels = labelDisco;
+  chartDisco.data.datasets[0].data = discoDashboard;
+
+  // Mandando os dados para os gráficos
+  chartDesempenhoCPU.update();
+  chartMemoria.update();
+  chartDisco.update();
+}
+
+/* for(i = 0; i < cpuDashboard.length; i ++) {
+  console.log('aaaaaa');
+  if(cpuDashboard[i] >= 75) {
+    chartDesempenhoCPU.backgroundColor = red;
+  }
+} */
+
+var chartDesempenhoCPU = new Chart(desempenhoCpu, {
+  type: 'line',
+  data: {
+      labels: labelDesempenhoCpu,
+      datasets: [
+          {
+              label: "",
+              backgroundColor: '#fff',
+              borderColor: '#38d6eb',
+              data: cpuDashboard,
+          }
+      ]
+  },
+  options: {
+      plugins: {
+        title: {
+          display: true,
+          text: 'Consumo da CPU',
+          padding: {
+            top: 10,
+          },
+          font: {
+            size: 18
           }
         }
+      }
+  }
+});
+
+
+var chartMemoria = new Chart(capacidadeMemoria, {
+  type: 'line',
+  data: {
+      labels: labelMemoria,
+      datasets: [
+          {
+        label: '',
+        backgroundColor: '#fff',
+        data: memoryDashboard,
+        fill: false,
+        borderColor: 'rgb(75, 192, 192)',
+
+      }
+  ]
+  },
+  options: {
+    plugins: {
+       title: {
+         display: true,
+         text: 'Consumo da Memória em Megabytes',
+         padding: {
+           top: 10,
+         },
+         font: {
+           size: 18
+         }
+       }
+    },
     }
 });
 
-var capacidadeMemoria = document.getElementById('chartMemoria').getContext('2d');
-var chart = new Chart(capacidadeMemoria, {
-    type: 'line',
-    data: {
-        labels: ['0 s', '10 s', '20 s', '30 s', '40 s', '50 s', '60 s'],
-        datasets: [
-            {
-          label: 'Uso da Memória',
-          backgroundColor: '#fff',
-          data: cpuDashboard,
-          fill: false,
-          borderColor: 'rgb(75, 192, 192)',
- 
-        }
-    ]
-    },
-    options: {
-        animations: {
-          tension: {
-            duration: 1000,
-            easing: 'linear',
-            from: 1,
-            to: 0,
-            loop: true
-          }
-        },
-        
-        scales: {
-          y: { 
-            min: 0,
-            max: 100
-          }
-        }
-      }
-});
+var chartDisco = new Chart(consumoDisco, {
+  type: 'line',
+  data: {
+      labels: labelDisco,
+      datasets: [
+          {
+        label: '',
+        backgroundColor: '#fff',
+        data: discoDashboard,
+        fill: false,
+        borderColor: 'rgb(75, 192, 192)',
 
-var capacidadeDisco = document.getElementById('chartDisco').getContext('2d');
-var chart = new Chart(capacidadeDisco, {
-    type: 'line',
-    data: {
-        labels: ['0 s', '10 s', '20 s', '30 s', '40 s', '50 s', '60 s'],
-        datasets: [
-            {
-          label: 'Uso de Armazenamento de Disco',
-          backgroundColor: '#fff',
-          data: cpuDashboard,
-          fill: false,
-          borderColor: 'rgb(75, 192, 192)',
- 
-        }
-    ]
-    },
-    options: {
-        animations: {
-          tension: {
-            duration: 1000,
-            easing: 'linear',
-            from: 1,
-            to: 0,
-            loop: true
-          }
-        },
-        
-        scales: {
-          y: { 
-            min: 0,
-            max: 100
-          }
-        }
       }
+  ]
+  },
+  options: {
+    plugins: {
+       title: {
+         display: true,
+         text: 'Consumo do Disco em Megabytes',
+         padding: {
+           top: 10,
+         },
+         font: {
+           size: 18
+         }
+       }
+    },
+    }
 });
--- site/src/models/dashboardModel.js
+++ site/src/models/dashboardModel.js
@@ -1,7 +1,7 @@
 var database = require("../database/config");
 
 function requisitarValoresCpuDatabase() {
-    instrucaoSql = `select consumoCPU from [dbo].[UsoDeMaquina]`;
+    instrucaoSql = `SELECT TOP (8) * FROM [dbo].[UsoDeMaquina] where Usuario = 5 order by hora DESC`;
     console.log("Executando a instrução SQL: \n"+ instrucaoSql);
     return database.executar(instrucaoSql);
 }
